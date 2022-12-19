3d6308e06d950e916ed6154dbb711fd1a0799140
Agustina Durigo <agustinadurigo@gmail.com>
2022-12-19 16:38:43 -0300

    Actualizo el componente Button

--- src/components/commons/Button/Button.jsx
+++ src/components/commons/Button/Button.jsx
@@ -2,7 +2,7 @@ import './_Button.scss'
 
 const Button = ({ children, variant }) => {
   return (
-    <button className={`button button-${variant}`}>{ children }</button>
+    <button className={`button button-${variant}`}>{children}</button>
   )
 }
 
