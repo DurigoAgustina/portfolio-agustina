25488602074d58e6690c91e2e4d6c914211acd14
Agustina Durigo <agustinadurigo@gmail.com>
2023-01-01 17:30:12 -0300

    Sección Portfolio modificada y finalizada.

--- src/components/layout/PortfolioSection/PortfolioSection.jsx
+++ src/components/layout/PortfolioSection/PortfolioSection.jsx
@@ -1,14 +1,14 @@
-import Card from '../../commons/Card/Card'
+
 import Title from '../../commons/Title/Title'
+import GridCard from '../../layout/GridCard/GridCard'
 import './_PortfolioSection.scss'
 
 const PortfolioSection = () => {
+
   return (
     <div className='container portfolio' id='proyectos'>
-      <Title Type='h2'>Proyectos</Title>
-      <Card />
-      <Card flexDirection='rowReverse'/>
-      <Card />
+      <Title Type='h1'>Proyectos</Title>
+      <GridCard />
     </div>
   )
 }
