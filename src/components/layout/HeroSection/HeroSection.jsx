fe5a0a6b3f87ab9e2339d3d108687ac2f62c9384
Agustina Durigo <agustinadurigo@gmail.com>
2023-01-01 17:30:27 -0300

    Sección HeroSection modificada y finalizada.

--- src/components/layout/HeroSection/HeroSection.jsx
+++ src/components/layout/HeroSection/HeroSection.jsx
@@ -3,7 +3,6 @@ import developer from '../../../assets/developer.svg'
 import './_HeroSection.scss'
 import Button from '../../commons/Button/Button'
 import CV from '../../../../public/CV-AGUSTINA-DURIGO.pdf'
-import Paragraph from '../../commons/Paragraph/Paragraph'
 
 const HeroSection = () => {
   return (
@@ -11,9 +10,6 @@ const HeroSection = () => {
         <div className='hero__title'>
           <Title Type='h3' color='brand'>FRONTEND DEVELOPER</Title>
           <Title Type='h1'>Agustina Durigo</Title>
-          <div className='hero__paragraph'>
-            <Paragraph>Short text with details about you, what you do or your professional career. You can add more information on the about page.</Paragraph>
-          </div>
           <div className='hero__button'>
             <Button variant='primary'> <a href='#proyectos'>Proyectos</a></Button>
             <a href={CV} download='CV-Agustina-Durigo'><Button variant='outline'>Descargar CV</Button></a>
