b674893f4819eece8462164f02ae670e9baa3389
Agustina Durigo <agustinadurigo@gmail.com>
2023-03-01 09:54:43 -0300

    Creo el archivo cards.js que contiene los proyectos realizados

---
+++ src/utils/cards.js
@@ -0,0 +1,26 @@
+import ProyectoTienda from '../assets/proyecto-tienda.png'
+import MercadoLibre from '../assets/mercadolibre-clon.png'
+
+const cards = [
+  {
+    id: 1,
+    title: 'Store App',
+    image: ProyectoTienda,
+    href: 'https://storeapp-adurigo.netlify.app/'
+  },
+  {
+    id: 2,
+    title: 'Clon de Mercado Libre',
+    image: MercadoLibre,
+    href: 'https://mercadolibre-clon.netlify.app/'
+  },
+  {
+    id: 3,
+    title: 'Nombre del proyecto 3',
+    image: ProyectoTienda,
+    href: '/'
+  }
+]
+
+
+export default cards;
