bafd0285d57aa296214cc51286fb8e19d5a7cb63
Agustina Durigo <agustinadurigo@gmail.com>
2023-03-01 09:52:16 -0300

    Creo menu.js que contiene un array de objetos

---
+++ src/utils/menu.js
@@ -0,0 +1,22 @@
+export const menu = [
+  {
+    id: 1,
+    category: 'Inicio',
+    href: 'inicio'
+  },
+  {
+    id: 2,
+    category: 'Sobre mí',
+    href: 'sobre-mi'
+  },
+  {
+    id: 3,
+    category: 'Proyectos',
+    href: 'proyectos'
+  },
+  {
+    id: 4,
+    category: 'Contacto',
+    href: 'contacto'
+  },
+]
